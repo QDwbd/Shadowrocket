043910944888ada2938d2f9b86a4b4e3735ba4bf
QDwbd <qingdeng1023@gmail.com>
2026-02-27 23:36:54 +0800

    Initial commit

---
+++ backend/tauri/src/config/mod.rs
@@ -0,0 +1,11 @@
+mod clash;
+mod core;
+mod draft;
+pub mod shadowrocket;
+mod prfitem;
+mod profiles;
+mod runtime;
+
+pub use self::{clash::*, core::*, draft::*, prfitem::*, profiles::*, runtime::*};
+
+pub use self::shadowrocket::IVerge;
